ba24212e7ee171d7ffbada640914ea2386f741d5
Carlos Jara Almendra <carlos.jaraa@sansano.usm.cl>
2020-07-19 20:29:24 -0400

    winduxd

--- src/main/java/Opelcorsa/demo/Quimioterapia/Quimioterapia.java
+++ src/main/java/Opelcorsa/demo/Quimioterapia/Quimioterapia.java
@@ -1,5 +1,40 @@
 package Opelcorsa.demo.Quimioterapia;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Quimioterapia {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private int piso;
+    private int numero;
+    
+    public Quimioterapia() {
+		super();
+	} 
+    
+	public long getId() {
+		return id;
+	}
+	public void setId(final long id) {
+		this.id = id;
+	}
+	public int getPiso() {
+		return piso;
+	}
+	public void setPiso(final int piso) {
+		this.piso = piso;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(final int numero) {
+		this.numero = numero;
+	}
     
 }
+
---
+++ src/main/java/Opelcorsa/demo/Quimioterapia/QuimioterapiaRepository.java
@@ -0,0 +1,9 @@
+package Opelcorsa.demo.Quimioterapia;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuimioterapiaRepository extends CrudRepository<Quimioterapia, Long> {
+	
+}
