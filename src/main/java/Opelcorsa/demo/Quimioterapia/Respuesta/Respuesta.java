3bb63ad16a46eb066990a06995062888e621d6e0
furry <kurry@DESKTOP-JSOSGN7.localdomain>
2020-07-26 22:19:18 -0400

    Se añadio Respuesta

---
+++ src/main/java/Opelcorsa/demo/Quimioterapia/Respuesta/Respuesta.java
@@ -0,0 +1,17 @@
+package Opelcorsa.demo.Quimioterapia.Respuesta;
+
+public class Respuesta {
+    private boolean ok;
+
+    public Respuesta(){
+        super();
+    }
+    public boolean isOk() {
+        return ok;
+    }
+
+    public void setOk(boolean ok) {
+        this.ok = ok;
+    }
+
+}
