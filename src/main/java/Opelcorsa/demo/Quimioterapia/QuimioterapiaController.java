4e574bde5aa270401c892ccb6af6b9af06e7a73e
BSolar.V <bastian.solar.v@gmail.com>
2020-08-08 23:55:33 -0400

    Heroku Database

--- src/main/java/Opelcorsa/demo/Quimioterapia/QuimioterapiaController.java
+++ src/main/java/Opelcorsa/demo/Quimioterapia/QuimioterapiaController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,7 +21,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/quimioterapia")
 public class QuimioterapiaController {
--- src/main/java/Opelcorsa/demo/Sillon/SillonControlller.java
+++ src/main/java/Opelcorsa/demo/Sillon/SillonControlller.java
@@ -3,6 +3,7 @@ package Opelcorsa.demo.Sillon;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import Opelcorsa.demo.Respuesta.Respuesta;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/quimioterapia/sillones")
 public class SillonControlller {
